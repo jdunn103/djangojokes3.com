67751117b2940884e8a64bc0dcf2d50f8727922b
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-07 15:22:50 -0400

    Created pages app and added templates for it.

---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+class AboutUsView(TemplateView):
+    template_name = 'pages/about_us.html'
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
