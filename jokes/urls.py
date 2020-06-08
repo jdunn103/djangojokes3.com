0aeaffced62b8cb0afcafe2c973e51485fe75dfb
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-08 19:14:03 -0400

    Added creating and updating capacities to the page

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import JokeDetailView, JokeListView
+from .views import (JokeCreateView, JokeDetailView, JokeListView,
+                    JokeUpdateView)
 
 urlpatterns = [
+    path('joke/<int:pk>/update', JokeUpdateView.as_view(), name='joke-update'),
+    path('joke/create/', JokeCreateView.as_view(), name='joke-create'),
     path('joke/<int:pk>/', JokeDetailView.as_view(), name='joke'),
     path('', JokeListView.as_view(), name='jokes'),
 ]
--- jokes/views.py
+++ jokes/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
 
 class JokeListView(ListView):
     model = Joke
@@ -8,3 +13,8 @@ class JokeListView(ListView):
 
 class JokeDetailView(DetailView):
     model = Joke
+    
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
