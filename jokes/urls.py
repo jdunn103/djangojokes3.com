6979334e920019b0cbb6706ff16e6579b61c81b0
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-12 16:06:54 -0400

    Added unique slugs

--- jokes/urls.py
+++ jokes/urls.py
@@ -4,9 +4,9 @@ from .views import (JokeCreateView, JokeDeleteView, JokeDetailView, JokeListView
                     JokeUpdateView)
 
 urlpatterns = [
-    path('joke/<int:pk>/update', JokeUpdateView.as_view(), name='joke-update'),
-    path('joke/<int:pk>/delete', JokeDeleteView.as_view(), name='joke-delete'),
+    path('joke/<slug>/update', JokeUpdateView.as_view(), name='joke-update'),
+    path('joke/<slug>/delete', JokeDeleteView.as_view(), name='joke-delete'),
     path('joke/create/', JokeCreateView.as_view(), name='joke-create'),
-    path('joke/<int:pk>/', JokeDetailView.as_view(), name='joke'),
+    path('joke/<slug>/', JokeDetailView.as_view(), name='joke'),
     path('', JokeListView.as_view(), name='jokes'),
 ]
