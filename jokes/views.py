2c87fc5ead14d1110055ce69cd741c50f05a622f
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-14 18:46:52 -0400

    Changed updates and create to use form_class

--- jokes/views.py
+++ jokes/views.py
@@ -2,11 +2,12 @@ from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, 
                                   ListView, UpdateView)
 
+from .forms import JokeForm
 from .models import Joke
 
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
 
 
 class JokeDeleteView(DeleteView):
@@ -24,4 +25,4 @@ class JokeListView(ListView):
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
