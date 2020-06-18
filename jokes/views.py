a630e690e201b40336ef1dd7e758613df1a3624a
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-18 14:26:05 -0400

    Made it so that users can only edit their jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView, DeleteView, DetailView, 
                                   ListView, UpdateView)
@@ -5,7 +6,7 @@ from django.views.generic import (CreateView, DeleteView, DetailView,
 from .forms import JokeForm
 from .models import Joke
 
-class JokeCreateView(CreateView):
+class JokeCreateView(LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
 
@@ -14,9 +15,13 @@ class JokeCreateView(CreateView):
         return super().form_valid(form)
 
 
-class JokeDeleteView(DeleteView):
+class JokeDeleteView(UserPassesTestMixin, DeleteView):
     model = Joke
     success_url = reverse_lazy("jokes")
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
 
 
 class JokeDetailView(DetailView):
@@ -27,6 +32,10 @@ class JokeListView(ListView):
     model = Joke
     
 
-class JokeUpdateView(UpdateView):
+class JokeUpdateView(UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+
+    def test_func(self):
+        obj = self.get_object()
+        return self.request.user == obj.user
