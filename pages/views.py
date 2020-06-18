fd4c95ce94c017fc09734e14d47c972db4256b4c
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-18 14:52:41 -0400

    Added Message styles to main.css

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,17 @@
+from django.contrib import messages
 from django.views.generic import TemplateView
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+    # def get(self, request, *args, **kwargs):
+    #     messages.debug(request, 'Debug message.')
+    #     messages.info(request, 'Info message.')
+    #     messages.success(request, 'Success message.')
+    #     messages.warning(request, 'Warning message.')
+    #     messages.error(request, 'Error message.')
+    #     return super().get(request, args, kwargs)
+
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
