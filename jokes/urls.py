6358e61dfbc3b89ede64731c2b15fef57bfefe0a
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-06-23 15:52:00 -0400

    Added vote view to URLConf

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import (JokeCreateView, JokeDeleteView, JokeDetailView, JokeListView,
-                    JokeUpdateView)
+                    JokeUpdateView, vote)
 
 urlpatterns = [
     path('joke/<slug>/update', JokeUpdateView.as_view(), name='joke-update'),
     path('joke/<slug>/delete', JokeDeleteView.as_view(), name='joke-delete'),
     path('joke/create/', JokeCreateView.as_view(), name='joke-create'),
     path('joke/<slug>/', JokeDetailView.as_view(), name='joke'),
+    path('joke/<slug>/vote/', vote, name='ajax-joke-vote'),
     path('', JokeListView.as_view(), name='jokes'),
 ]
