94bd01393f7676b67ccd051f05a5b4df35a25ea4
jdunn103 <48596653+jdunn103@users.noreply.github.com>
2020-07-08 17:32:25 -0400

    Updated Admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
+
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from common.admin import DjangoJokesAdmin
 
@@ -25,7 +29,17 @@ class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
     add_fieldsets = append_fields(
         UserAdmin.add_fieldsets, 'Optional Fields', optional_fields
     )
+    readonly_fields = ['password_form']
+    fieldsets = append_fields(UserAdmin.fieldsets, None, ('password_form',))
 
     def get_form(self, request, obj=None, **kwargs):
         self.save_on_top = obj is not None
         return super().get_form(request, obj, **kwargs)
+
+    def password_form(self, obj):
+        url = reverse('admin:auth_user_password_change', args=[obj.pk])
+        return mark_safe(f'<a href="{url}">Change Password</a>')
+
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialToken)
